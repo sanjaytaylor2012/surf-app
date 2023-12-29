04ba50ecc25e1db793d5126c49ceedafbcafbade
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-12-28 22:40:39 -0500

    asd

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
+import React from "react";
 
-export const SignUp = () => {
-  redirect("/sign-in");
-
-  return <div>Sign in</div>;
+const SignUpPage: React.FC = () => {
+  return (
+    <div className="flex w-full h-full items-center justify-center">
+      <Button onClick={() => redirect("/sign-in")}>Sign in</Button>
+    </div>
+  );
 };
+export default SignUpPage;
